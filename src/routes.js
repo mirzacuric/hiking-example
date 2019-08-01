20d85fde3c6cea1ff3409977f31cb4e7328ef26f
Mirza Curic <mirza.curic@stu.ibu.edu.ba>
2019-08-01 15:08:53 +0200

    Added JOI validation for destination request payload

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,22 @@
 import { Router } from 'express';
+import Joi from 'joi';
+import payloadValidator from './middlewares/payloadValidator';
 import DestinationController from './controllers/DestinationController';
 
 const destinationController = new DestinationController();
 
+const schemas = {
+  points: Joi.array().items(
+    Joi.array().items(Joi.number().positive())
+  ).min(2).max(1000)
+}
+
 const router = Router();
 
 export default [
-  router.post('/destination/findHighestValue', destinationController.findHighestValue)
+  router.post(
+    '/destination/findHighestValue',
+    payloadValidator(schemas.points, 'points'),
+    destinationController.findHighestValue
+  )
 ]
